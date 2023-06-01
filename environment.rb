66ad7b178651dd9b8a023e20f84299c90ce21d81
Meshack Korir <meshackkorir100@gmail.com>
2023-06-01 14:53:29 +0300

    done with environment file

--- environment.rb
+++ environment.rb
@@ -1,1 +1,10 @@
 # This one will link the DB to ruby and VS
+require "bundler"
+Bundle.require
+
+#add the class files
+require_relative `students.rb`
+
+#connect db to ruby
+
+DB = {conn: SQLite3::Database.new("student.db")}
